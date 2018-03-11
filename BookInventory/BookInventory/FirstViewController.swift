5dd561d89966a4d730d133c5e7c2db0da6c63745
NyeinEi <pauksohne2t@gmail.com>
2018-03-11 23:28:41 +0800

    Added pod files, TabbarControllers

---
+++ BookInventory/BookInventory/FirstViewController.swift
@@ -0,0 +1,25 @@
+//
+//  FirstViewController.swift
+//  BookInventory
+//
+//  Created by Moe Han on 3/11/18.
+//  Copyright © 2018 NyeinEi. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
